3eb7e1cf0065f1b6ebae092095f48dd05d2b5ec7
mubarak23 <mubarakaminu340@gmail.com>
2019-07-21 00:04:04 +0100

    setup mlab db

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb://root:root123@ds251240.mlab.com:51240/nodehome"
+}
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodypaser = require('body-parser');
- 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
+const db = require('./config/db');
 
-require('./app/routes')(app, {});
+const app = express();
+app.use(bodypaser.urlencoded({ extended: true}));
 const port = 3000;
+MongoClient.connect(db.url, (err, database)=>{
+    if(err) return console.log(err);
+    require('./app/routes')(app, {database});
 app.listen(port, ( )=>{
     console.log('we are live on port' + port);
+    })
+
+    
 })
+
+

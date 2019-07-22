dc7a61d55d1a5daaa3a58717f8653d225ba92b42
mubarak23 <mubarakaminu340@gmail.com>
2019-07-22 09:58:36 +0100

    add validation to the update, and delete

--- app/routes/project_route.js
+++ app/routes/project_route.js
@@ -34,12 +34,12 @@ module.exports = function(app, db){
      db.collection("projects").insert(project, (err, result) =>{
          if(err){
              //res.send({"error": "An error has Occured"});
-             return res.status(400).send({
+             return res.status(401).send({
                  success: false,
                  message: 'Internal Server Error'
              });
          }else{
-             return res.status(400).send({
+             return res.status(200).send({
                  success: true,
                  message: 'New Project Created',
                  data: result.ops[0]
@@ -51,27 +51,54 @@ module.exports = function(app, db){
 //get a single project detail route
 app.get('/api/v1/project/:id', (req, res) =>{
      const id = req.params.id;
-     
+     if(!id){
+         return res.status(400).send({
+             success: false,
+             message: 'id is required for this request'
+         })
+     }
      const details = {'_id':new ObjectID(id)};
      db.collection('projects').findOne(details, (err, item) =>{
          if(err){
-             send.res({'error': 'An Error Has Occur'})
+             //send.res({'error': 'An Error Has Occur'})
+             return res.status(401).send({
+                 success: false,
+                 message: 'Internal Server Error'
+             });
          }else{
-             res.send(item);
+             return res.status(200).send({
+                 success:true,
+                 message: 'project details',
+                 data: item
+             });
+             //res.send(item);
          }
-
      });
 });
 
 //delete project route
 app.delete('/api/v1/project/:id', (req, res) =>{
     const id = req.params.id;
+    if(!id){
+        return res.status(400).send({
+            success: false,
+            message: 'id is required'
+        });
+    }
     const details = {'_id': new ObjectID(id)};
     db.collection('projects').remove(details, (err, item) =>{
         if(err){
-            send.res({'error': 'An Error Has Occur'});
+            //send.res({'error': 'An Error Has Occur'});
+            return res.status(401).send({
+                success: false,
+                message: 'Internal server Error'
+            });
         }else{
-            res.send('Project ' + id + ' Deleted');
+            return res.status(200).send({
+                success: true,
+                message: 'project with id' + id + 'Deleted Successfully' 
+            });
+            //res.send('Project ' + id + ' Deleted');
         }
     });
 })
@@ -79,6 +106,12 @@ app.delete('/api/v1/project/:id', (req, res) =>{
 //update project
 app.put('/api/v1/project/:id', (req, res) =>{
     const id = req.params.id;
+    if(!id){
+        return res.status(400).send({
+            success: false,
+            message: 'id is required for this request'       
+        });
+    }
     const details = {'_id': new ObjectID(id)};
     const project_update = {
         title: req.body.title,
@@ -89,9 +122,19 @@ app.put('/api/v1/project/:id', (req, res) =>{
     };
     db.collection('projects').updateOne(details, project_update, (err, item) =>{
         if(err){
-            res.send({'error': 'An Error Has Occured'})
+            return res.status(401).send({
+                success: false,
+                message:'Internal Server Error' 
+            });
+
+            //res.send({'error': 'An Error Has Occured'})
         }else{
-            res.send(item);
+            return res.status(200).send({
+                success:true,
+                message: 'Project with id ' + id + 'Updated Successfully',
+                data: item
+            });
+            //res.send(item);
         }
     })
 })

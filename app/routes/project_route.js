aaede1d572eda963179aaff52604b02a5c6b3bde
mubarak23 <mubarakaminu340@gmail.com>
2019-07-22 09:08:59 +0100

    add validation for post request

--- app/routes/project_route.js
+++ app/routes/project_route.js
@@ -1,6 +1,30 @@
 var ObjectID = require('mongodb').ObjectID; 
 module.exports = function(app, db){
  app.post('/api/v1/project/create', (req, res) =>{
+     if(!req.body.title){
+         return res.status(400).send({
+             success: 'false',
+             message: 'title is required'
+         });
+     }
+     if(!req.body.description){
+         return res.status(400).send({
+             success: false,
+             message: 'description is require'
+         });
+     }
+     if(!req.body.assign_student){
+         return res.status(400).send({
+             success: false,
+             message: 'Assign student is required'
+         });
+     }
+     if(!req.body.assign_supervisor){
+         return res.status(400).send({
+             success: false,
+             message: 'Assign Supervisor is required'
+         });
+     }
      const project = {title: req.body.title,
          description:req.body.description,
         assign_student: req.body.assign_student,
@@ -9,15 +33,25 @@ module.exports = function(app, db){
         //let db = client.db("nodehome");
      db.collection("projects").insert(project, (err, result) =>{
          if(err){
-             res.send({"error": "An error has Occured"});
+             //res.send({"error": "An error has Occured"});
+             return res.status(400).send({
+                 success: false,
+                 message: 'Internal Server Error'
+             });
          }else{
-             res.send(result.ops[0]);
+             return res.status(400).send({
+                 success: true,
+                 message: 'New Project Created',
+                 data: result.ops[0]
+             }) 
+             //res.send(result.ops[0]);
          }
      });
  });
 //get a single project detail route
 app.get('/api/v1/project/:id', (req, res) =>{
      const id = req.params.id;
+     
      const details = {'_id':new ObjectID(id)};
      db.collection('projects').findOne(details, (err, item) =>{
          if(err){

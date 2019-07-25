50c061e15d329bb63412abe8c44dea2b531dd40e
mubarak23 <mubarakaminu340@gmail.com>
2019-07-25 14:10:01 +0100

    update user details route created

--- app/routes/meeting_route.js
+++ app/routes/meeting_route.js
@@ -64,4 +64,96 @@ module.exports  = function(app, db){
     });
 
 
+    app.get('/api/v1/meeting/:id', (req, res) =>{
+        const id = req.params.id;
+        if(!id){
+            return res.status(401).send({
+                success: false,
+                message : 'id is required'
+            })
+        }
+        const details = {'_id':new ObjectID(id)};
+        db.collection('meetings').findOne(details, (err, item) =>{
+            if(err){
+                //send.res({'error': 'An Error Has Occur'})
+                return res.status(401).send({
+                    success: false,
+                    message: 'Internal Server Error'
+                });
+            }else{
+                return res.status(200).send({
+                    success:true,
+                    message: 'meetings details',
+                    data: item
+                });
+                //res.send(item);
+            }
+        });        
+    });
+
+    //edit meeting route
+    app.put('/api/v1/meeting/:id', (req, res) =>{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).send({
+                success: false,
+                message: 'id is required for this request'       
+            });
+        }
+        const details = {'_id': new ObjectID(id)};
+        const meeting_update = {
+            date: req.body.date,
+            time: req.body.time,
+            location: req.body.location,
+            created_by: req.body.created_by,
+            with_student_by: req.body.with_student_by,
+            create_at: (new Date(Date.now()).toISOString())  
+        }
+        db.collection('meeting').updateOne(details, meeting_update, (err, item) =>{
+            if(err){
+                return res.status(401).send({
+                    success: false,
+                    message:'Internal Server Error' 
+                });
+    
+                //res.send({'error': 'An Error Has Occured'})
+            }else{
+                return res.status(200).send({
+                    success:true,
+                    message: 'Meeting with id ' + id + 'Updated Successfully',
+                    data: item
+                });
+                //res.send(item);
+            }
+        })
+    })
+
+
+    app.delete('/api/v1/meeting/:id', (req, res) =>{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).send({
+                success: false,
+                message: 'id is required'
+            });
+        }
+        const details = {'_id': new ObjectID(id)};
+        db.collection('projects').remove(details, (err, item) =>{
+            if(err){
+                //send.res({'error': 'An Error Has Occur'});
+                return res.status(401).send({
+                    success: false,
+                    message: 'Internal server Error'
+                });
+            }else{
+                return res.status(200).send({
+                    success: true,
+                    message: 'project with id' + id + 'Deleted Successfully' 
+                });
+                //res.send('Project ' + id + ' Deleted');
+            }
+        });
+    })   
+
+
 }
--- app/routes/user_route.js
+++ app/routes/user_route.js
@@ -121,5 +121,42 @@ app.delete('/api/v1/user/:id', (req, res) =>{
     });
 });
 
+app.put('/api/v1/user/:id', (req, res) =>{
+    const id = req.params.id;
+    if(!id){
+        return res.status(400).send({
+            success: false,
+            message: 'id is required for this request'       
+        });
+    }
+    const details = {'_id': new ObjectID(id)};
+    const update_user = {username: req.body.username,
+        fullname:req.body.fullname,
+       password: req.body.password,
+       email: req.body.email,
+       assign_to:req.body.assign_to,
+       reg_no: req.body.reg_no,
+       status:0,
+       create_date: (new Date(Date.now()).toISOString())};
+       //let db = client.db("nodehome");
+    db.collection("users").updateOne(details, update_user, (err, result) =>{
+        if(err){
+            //res.send({"error": "An error has Occured"});
+            return res.status(401).send({
+                success: false,
+                message: 'Internal Server Error'
+            });
+        }else{
+            return res.status(200).send({
+                success: true,
+                message: 'Project with id ' + id + 'Updated Successfully',
+                data: result.ops[0]
+            }) 
+            //send the email with a link to confirm his email address and login
+            //res.send(result.ops[0]);
+        }
+    });
+});
+
 
 }

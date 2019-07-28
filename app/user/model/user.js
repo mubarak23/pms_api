26d9798cbc23c91ef7a9e8f984761741a123e6e2
mubarak23 <mubarakaminu340@gmail.com>
2019-07-28 17:40:16 +0100

    refactore user schema using mongoose

---
+++ app/user/model/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userModel = new Schema({
+  email: { type: String, required: true, index: { unique: true } },
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+  admin: { type: Boolean, required: true },
+  assign_to: {type: String, required: true },
+  reg_no: {type: String, required: true},
+  fullname: {type:String, required: true}
+})
+
+module.exports = mongoose.Model('User', userModel);
+

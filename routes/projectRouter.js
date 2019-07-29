3b288621a28873a4aa8363caa432cab9cdcf58d4
mubarak23 <mubarakaminu340@gmail.com>
2019-07-30 00:10:01 +0100

    create the post and get project details route

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,6 +1,56 @@
 const express = require('express');
 const projectRouter = express.Router();
 
-function(Project){
-    
+function routes (Project){
+    projectRouter.route('/project')
+    .post((req, res) =>{
+        const project = new Project(req.body);
+        project.save();
+        return res.status(201).json(book);
+    })
+    .get((req, res) =>{
+        Project.find((err, projects) =>{
+            if(err){
+        const response = {
+            "message": 'Unable to find a book',
+            "success": false,
+            "status": 401
+        };
+        return res.status(401).json({
+            "message": "Unable to find a book",
+        });    
+         }        
+        
+       return res.status(200).json({
+           "message":"All Project from the pms",
+           "data": projects
+       });  
+        });
+    });
+
+
+    bookRouter.route('/project/:bookID')
+.get((req, res) =>{
+    const book_id = req.params.bookID;
+    //return book_id;
+    Book.findById(book_id, (error, book) =>{
+        if(error){
+            const response = {
+                "message": 'nable to find book with the targeted id',
+                "success": false,
+                "status": 401
+            };
+           return res.json(response);
+        }
+        const response = {
+            "message": ' Book with ID Retrieved',
+            "success": true,
+            "data": book,
+            "status": 200
+        };
+       return res.json(response);
+    })
+});
+return bookRouter;
+
 }

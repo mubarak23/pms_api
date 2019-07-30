e6b533a04dfc866c48fc8ebdfb8f5331c0e4366c
mubarak23 <mubarakaminu340@gmail.com>
2019-07-30 17:50:22 +0100

    post and get route for meeting route created

--- routes/meetingRouter.js
+++ routes/meetingRouter.js
@@ -1,2 +1,24 @@
 const express = express();
 const meetingRouter = express.Router();
+
+function routes(Meeting){
+    meetingRouter.route('/meeting')
+    .post((req, res) =>{
+        const meeting = new Meeting(req.body);
+        meeting.save();
+        return res.status(201).json(meeting);
+    })
+    .get((req, res) =>{
+        Meeting.find((error, meetings) =>{
+            if(error){
+                return res.status(401).json({
+                    "message": "Internal Server Error",
+                });
+            }
+            return res.status(401).json({
+                "message": "Unable to find a book",
+                "data": meetings
+            });
+        });
+    })
+}

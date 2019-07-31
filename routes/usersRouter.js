8f2d6287d8fa5212bcf70bee3687f9ede530440d
mubarak23 <mubarakaminu340@gmail.com>
2019-07-31 16:51:59 +0100

    createe add user and get user route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const userRouter = express.Router();
 
+function routes(User){
+    userRouter.route('/user')
+    .get((req, res) => {
+        User.find((error, users) =>{
+            if(error){
+              return res.status(401).json({
+                    "message": "Internal Server Error",
+                });
+            }
+             return res.status(401).json({
+                "message": "List of users",
+                "data": users
+            });
+        })
+    })
+    .post((req, res) =>{
+        const user = new User(req.body);
+        user.save();
+        return res.status(201).json(user);
+    })
+}

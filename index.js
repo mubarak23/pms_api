88901fad9d0058ad552bd158db4dae0495896b9a
mubarak23 <mubarakaminu340@gmail.com>
2019-08-15 00:07:21 +0100

    add header middleware

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const userRouter = require('./routes/usersRouter')(User);
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV = 'development'){
+    app.use((req, res, next) =>{
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Methods',
+            'POST, GET, PUT, DELETE, HEAD'
+        );
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Authorization. X-PINGOTHER, Origin, X-Requested-with, Content-Type, Accept, x-access-token'
+        );
+        next();
+    })
+}
+
 const db = mongoes.connect("mongodb://root:root123@ds251240.mlab.com:51240/nodehome",
  {useNewUrlParser: true },
   (error) =>{
@@ -33,6 +49,8 @@ app.use('/api', userRouter);
      res.send('Welcome to library resfull api');
  });
  
+
+
  app.listen(port, () =>{
      console.log('Running on port ' + port);
  })
